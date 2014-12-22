4895f161e668c44c4b0bedf589e44110079db3cd
kpeeples <kpeeples@localhost.localdomain>
2014-12-21 23:52:12 -0500

    changes for integration dynamic tab

--- scripts/app.js
+++ scripts/app.js
@@ -26,14 +26,15 @@ app.config(function ($routeProvider) {
     }).when('/dv',{
     	templateUrl: 'views/dv.html'
     }).when('/integrated',{
-    	templateUrl: 'views/integrated.html'
+    	templateUrl: 'views/integrated.html',
+	controller: 'INTEGRATIONCtrl'
     }).otherwise({
         redirectTo: '/'
     });
 });
 
 app.controller('TabsCtrl', ['$scope', function ($scope) {
-    $scope.tabs = [{title: 'Start',url: '#'},{title: 'Integrated Demos',url: '#integrated'}, {title: 'JBoss EAP Demos',url: '#eap'}, {title: 'DataGrid Demos',url: '#datagrid'}, {title: 'BPM Suite Demos',url: '#bpms'}, {title: 'BRMS Demos',url: '#brms'},{title: 'Fuse Demos',url: '#fuse'},{title: 'FSW Demos',url: '#fsw'},{title: 'DV Demos',url: '#dv'}];
+    $scope.tabs = [{title: 'Start',url: '#'},{title: 'Integration Demos',url: '#integrated'}, {title: 'JBoss EAP Demos',url: '#eap'}, {title: 'DataGrid Demos',url: '#datagrid'}, {title: 'BPM Suite Demos',url: '#bpms'}, {title: 'BRMS Demos',url: '#brms'},{title: 'Fuse Demos',url: '#fuse'},{title: 'FSW Demos',url: '#fsw'},{title: 'DV Demos',url: '#dv'}];
 
     $scope.currentTab = '#';
 
@@ -53,6 +54,21 @@ app.controller('JDGCtrl', function ($scope, $http) {
     	//filter on name
     	var demos = repos.filter(function(repo, status, headers, config) {
     		 return repo.name.toUpperCase().match("DATAGRID");
+    	});
+    	demos.forEach(function(demo) { getDemoConfig(demo,$http,$scope) });
+    	
+    }).error(function (data, status) {
+        console.log('Error ' + data);
+    });  
+});
+
+
+app.controller('INTEGRATIONCtrl', function ($scope, $http) {
+    $scope.demos = new Array();
+	$http.get('https://api.github.com/orgs/jbossdemocentral/repos?per_page=200').success(function (repos) {
+    	//filter on name
+    	var demos = repos.filter(function(repo, status, headers, config) {
+    		 return repo.name.toUpperCase().match("INTEGRATION");
     	});
     	demos.forEach(function(demo) { getDemoConfig(demo,$http,$scope) });
     	
